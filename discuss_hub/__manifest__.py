57958cfd7081422cf85dc0f7f834c32067086a85
Duda Nogueira <dudanogueira@gmail.com>
2025-10-17 16:31:55 -0300

    Add a group to control access to Discuss Hub

--- discuss_hub/__manifest__.py
+++ discuss_hub/__manifest__.py
@@ -12,7 +12,7 @@
     "depends": ["base", "mail", "base_automation", "crm"],
     # always loaded
     "data": [
-        #'security/security.xml',
+        "security/security.xml",
         "security/ir.model.access.csv",
         "views/views.xml",
         "views/templates.xml",
